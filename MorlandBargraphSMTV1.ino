beea1462c259cc04b83fea95b06761a8a4c96132
CRDArcy <cdarcy@rsgc.on.ca>
2019-02-14 10:09:07 -0500

    Test Code for MorlandBargraphSMTV1

---
+++ MorlandBargraphSMTV1.ino
@@ -0,0 +1,87 @@
+// PROJECT  :MorlandBargraphSMTV1
+// PURPOSE  :To confirm Morland Bargraph SMT version 1
+// DATE     :2019 02 14
+// STATUS   :Working
+#define dataPin 13
+#define clockPin 12
+#define latchPin 11
+#define enablePin 3
+#define DELAYMS 10
+#define R2L 0
+#define L2R 1
+#define LEDs 24
+uint32_t UPPER = 1L << LEDs;
+uint32_t n = 1;
+
+void setup() {
+  pinMode(dataPin, OUTPUT);   //
+  pinMode(clockPin, OUTPUT);  //declare pins for output
+  pinMode(latchPin, OUTPUT);  //
+  pinMode(enablePin, OUTPUT);  //
+  digitalWrite(enablePin, LOW);
+  
+  //uncomment one of the four presentations below
+  shoot(L2R, DELAYMS);
+  //displayRandom();
+  //displayAll();
+  //breathe(5);
+}
+
+//utility function used by most (all?) animations below
+void shift(uint32_t n) {
+  digitalWrite(latchPin, LOW);                      //pull latch LOW
+  shiftOut(dataPin, clockPin, MSBFIRST, n >> 16);   //upload byte 2 data
+  shiftOut(dataPin, clockPin, MSBFIRST, n >> 8);    //upload byte 1 data
+  shiftOut(dataPin, clockPin, MSBFIRST, n );        //upload byte 0  data
+  digitalWrite(latchPin, HIGH);                     //present bits on output pins
+}
+
+//depending on the direction parameter, single LEDs will fly across the entire range
+//from right to left (or left to right), at a rate determined by the second parameter
+void shoot(uint8_t dir, uint16_t duration) {
+  while (true)
+    if (dir == R2L) {
+      n = 1;
+      while (n < UPPER) {
+        shift(n);
+        delay(duration);
+        n <<= 1;
+      }
+    }
+    else {
+      n = UPPER >> 1;
+      while (n > 0) {
+        shift(n);
+        delay(duration);    //admire...
+        n >>= 1;            //adjust n
+      }
+    }
+}
+
+void displayRandom() {
+  while (true) {
+    n = random(UPPER);
+    shift(n);
+    delay(DELAYMS << 3);
+  }
+}
+
+
+void displayAll() {
+  n = UPPER - 1;                    //prepare to light 'em all!
+  shift(n);                         //do it
+}
+
+void breathe(uint8_t duration) {
+  displayAll();                     //light the entire range of LEDs
+  uint8_t level = 0;                //start with output enable
+  int8_t delta = 1;                 //increment decrement of PWM on OE' pin (3)
+  while (true) {                    //repeat, indefinitely
+    analogWrite(enablePin, level);  //Set the PWM duty cycle
+    delay(duration);                //admire...
+    level += delta;                 //adjust duty cycle (either increase or decrease)
+    delta = level == 255 || level == 0 ? -delta : delta;  //change direction, if necessary
+  }
+}
+
+void loop() {}
